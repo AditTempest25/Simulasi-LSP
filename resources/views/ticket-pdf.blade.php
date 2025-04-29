4527c6fd1fb82db5e4b4e3b9fee933aaf87bd5e9
AditTempest25 <adityaputra251007@gmail.com>
2025-04-29 13:44:16 +0700

    enhance dkit

--- resources/views/ticket-pdf.blade.php
+++ resources/views/ticket-pdf.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link href="https://fonts.googleapis.com/css2?family=Libre+Barcode+128&family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Libre+Barcode+128&family=Poppins:wght@300;400;600&display=swap"
+        rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/jsbarcode@3.11.0/dist/JsBarcode.all.min.js"></script>
     <style>
         body {
@@ -109,8 +110,18 @@
 <body>
     <div class="ticket-container">
         <div class="header">
-            <h1>SkyLine Penerbangan</h1>
+            <div class="header" style="display: flex; align-items: center; justify-content: center; gap: 10px;">
+                <!-- Logo SVG -->
+                <svg style="height: 40px; width: 40px; color: #3b82f6;" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M12 2L3 9L6 10L8 22L12 20L16 22L18 10L21 9L12 2Z" fill="currentColor" />
+                    <path d="M12 2V20" stroke="white" stroke-width="2" />
+                </svg>
+                <!-- Teks Judul -->
+                <h1 style="font-size: 32px; font-weight: 600; color: #3f51b5; margin: 0;">SkyLine Penerbangan</h1>
+            </div>
             <h2>{{ $ticket->nama_maskapai }}</h2>
+
+
         </div>
 
         <div class="ticket-info">
@@ -159,10 +170,10 @@
         JsBarcode("#barcode", "{{ $ticket->orderTiket->no_struk }}", {
             format: "CODE128", // Format barcode, bisa diganti sesuai keinginan
             lineColor: "#0aa", // Warna garis barcode
-            width: 4,          // Lebar garis
-            height: 100,       // Tinggi barcode
+            width: 4, // Lebar garis
+            height: 100, // Tinggi barcode
             displayValue: true, // Menampilkan nilai di bawah barcode
-            fontSize: 18,      // Ukuran font untuk teks di bawah barcode
+            fontSize: 18, // Ukuran font untuk teks di bawah barcode
         });
     </script>
 </body>
